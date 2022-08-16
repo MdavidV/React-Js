6dd585d9aee91146b86e1e38727620d0b30a410c
David <vinaarizadavid@gmail.com>
2022-08-16 07:58:28 -0500

    creating app

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          No se de que voy a hacer el ecommerce :D
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Repositorio
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
