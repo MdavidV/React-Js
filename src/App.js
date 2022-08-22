94a25aead017e2c74eef6cad20a602632de74fe4
David <vinaarizadavid@gmail.com>
2022-08-21 23:31:25 -0500

    segunda entrega

--- src/App.js
+++ src/App.js
@@ -1,31 +1,17 @@
 // import logo from "./logo.svg";
 import "./App.css";
 
+import MenuNav from "./components/navBar";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <nav className="Menu-nav">
-          <h1 className="Logo">Lorem Ipsum</h1>
-
-          <div className="Anchors-nav">
-            <a href="index.html" className="item-menu-Nav">
-              Home
-            </a>
-            <a href="index.html" className="item-menu-Nav">
-              Products
-            </a>
-            <a href="index.html" className="item-menu-Nav">
-              Contact us!
-            </a>
-          </div>
-        </nav>
+        <MenuNav />
       </header>
 
       <body>
-        <p>
-          Estoy trabajando en el disenio de la pagina haha
-        </p>
+        <p>Estoy trabajando en el disenio de la pagina haha</p>
       </body>
     </div>
   );
