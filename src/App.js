25c84d58c59d3a55f4848f819ac2c2be2a73caf6
David <vinaarizadavid@gmail.com>
2022-08-21 23:13:59 -0500

    segundo Desafio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          No se de que voy a hacer el ecommerce :D
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Repositorio
-        </a>
+        <nav className="Menu-nav">
+          <h1 className="Logo">Lorem Ipsum</h1>
+
+          <div className="Anchors-nav">
+            <a href="index.html" className="item-menu-Nav">
+              Home
+            </a>
+            <a href="index.html" className="item-menu-Nav">
+              Products
+            </a>
+            <a href="index.html" className="item-menu-Nav">
+              Contact us!
+            </a>
+          </div>
+        </nav>
       </header>
+
+      <body>
+        <p>
+          Estoy trabajando en el disenio de la pagina haha
+        </p>
+      </body>
     </div>
   );
 }
